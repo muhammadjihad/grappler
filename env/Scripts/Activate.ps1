3a0ed6b630c3fcb83d681db487de51aca40fdedf
jihad <abbdurahmanj@gmail.com>
2019-02-28 23:53:19 +0800

    update pip

--- env/Scripts/Activate.ps1
+++ env/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="D:\Belajar Programming\proyek\env"
+$env:VIRTUAL_ENV="D:\belajarprogramming\proyek\env"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
