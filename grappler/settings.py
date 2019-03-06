15887acdcca0c6c658d8c7302fa28ed82476a91c
muhammadjihad <45728460+muhammadjihad@users.noreply.github.com>
2019-03-06 17:15:17 +0700

    gettingout some stuff

--- grappler/settings.py
+++ grappler/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9&a2@#lu9nlxn(b$7hl^b&bx(&nlbst-1o=)xxd%z%gsdr4v^n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['grappler.herokuapp.com']
 
